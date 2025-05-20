ebac9b98903c71bdd3cd6bb485f5b133a4b321d2
OlhaOA <olalaadka@gmail.com>
2025-05-20 11:49:42 +0300

    add read.html, jinja2

--- main.py
+++ main.py
@@ -1,54 +1,59 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-import urllib.parse, pathlib, mimetypes, os, json
+import urllib.parse, pathlib, mimetypes, json
 from datetime import datetime
+from jinja2 import Environment, FileSystemLoader
+
 
 class HttpHandler(BaseHTTPRequestHandler):
+    env = Environment(loader=FileSystemLoader("."))
+
     def do_GET(self):
         pr_url = urllib.parse.urlparse(self.path)
-        if pr_url.path == '/':
-            self.send_html_file('index.html')
-        elif pr_url.path == '/message':
-            self.send_html_file('message.html')
+        if pr_url.path == "/":
+            self.send_html_file("index.html")
+        elif pr_url.path == "/message":
+            self.send_html_file("message.html")
+        elif pr_url.path == "/read":
+            self.render_messages()
         else:
             if pathlib.Path().joinpath(pr_url.path[1:]).exists():
                 self.send_static()
             else:
-                self.send_html_file('error.html', 404)
+                self.send_html_file("error.html", 404)
 
     def do_POST(self):
-        data = self.rfile.read(int(self.headers['Content-Length']))
+        data = self.rfile.read(int(self.headers["Content-Length"]))
         data_parse = urllib.parse.unquote_plus(data.decode())
-        data_dict = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+        data_dict = {
+            key: value for key, value in [el.split("=") for el in data_parse.split("&")]
+        }
 
-        file_path = pathlib.Path('storage/data.json')
+        file_path = pathlib.Path("storage/data.json")
         if file_path.exists():
-                with open(file_path, 'r', encoding='utf-8') as f:
-                    try:
-                        all_data = json.load(f)
-                    except json.JSONDecodeError:
-                        all_data = {}
+            with open(file_path, "r", encoding="utf-8") as f:
+                try:
+                    all_data = json.load(f)
+                except json.JSONDecodeError:
+                    all_data = {}
         else:
             all_data = {}
 
-        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
         all_data[timestamp] = data_dict
 
-        with open(file_path, 'w', encoding='utf-8') as f:
+        with open(file_path, "w", encoding="utf-8") as f:
             json.dump(all_data, f, ensure_ascii=False, indent=4)
 
         self.send_response(302)
-        self.send_header('Location', '/')
+        self.send_header("Location", "/")
         self.end_headers()
-        
-                    
 
     def send_html_file(self, filename, status=200):
         self.send_response(status)
-        self.send_header('Content-type', 'text/html')
+        self.send_header("Content-type", "text/html")
         self.end_headers()
-        with open(filename, 'rb') as fd:
+        with open(filename, "rb") as fd:
             self.wfile.write(fd.read())
-
 
     def send_static(self):
         self.send_response(200)
@@ -56,19 +61,40 @@ class HttpHandler(BaseHTTPRequestHandler):
         if mt:
             self.send_header("Content-type", mt[0])
         else:
-            self.send_header("Content-type", 'text/plain')
+            self.send_header("Content-type", "text/plain")
         self.end_headers()
-        with open(f'.{self.path}', 'rb') as file:
+        with open(f".{self.path}", "rb") as file:
             self.wfile.write(file.read())
+
+    def render_messages(self):
+        file_path = pathlib.Path("storage/data.json")
+        if file_path.exists():
+            with open(file_path, "r", encoding="utf-8") as f:
+                try:
+                    messages = json.load(f)
+                except json.JSONDecodeError:
+                    messages = {}
+        else:
+            messages = {}
+
+        template = self.env.get_template("read.html")
+        output = template.render(messages=messages)
+
+
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(output.encode("utf-8"))
 
 
 def run(server_class=HTTPServer, handler_class=HttpHandler):
-    server_address = ('', 3000)
+    server_address = ("", 3000)
     http = server_class(server_address, handler_class)
     try:
         http.serve_forever()
     except KeyboardInterrupt:
         http.server_close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     run()
